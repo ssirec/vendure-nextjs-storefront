956f02bb121622ea3e8fd072c5ff6e12f4cec4b2
ssirec <125913769+ssirec@users.noreply.github.com>
2024-11-05 21:50:11 +0100

    Update [code].page.tsx

--- src/pages/checkout/confirmation/[code].page.tsx
+++ src/pages/checkout/confirmation/[code].page.tsx
@@ -1,3 +1,10 @@
+export const runtime = 'edge';
+
+const CheckoutConfirmation = () => {
+  // Your component code
+};
+
+export default CheckoutConfirmation;
 import Page, { getServerSideProps } from '@/src/pages/[channel]/checkout/confirmation/[code].page';
 import { Redirect } from '@/src/lib/redirect';
 import React from 'react';
